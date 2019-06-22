b917cc2ce7a13035e8daf57b90370df8dab6be91
Huhu0731 <you@example.com>
2019-06-22 19:39:22 +0800

    发送验证码-请求获取初始化验证码参数

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -14,11 +14,11 @@
               <el-input v-model="form.code" placeholder="验证码"></el-input>
             </el-col>
             <el-col :span="10" :offset="2">
-              <el-button>获取验证码</el-button>
+              <el-button @click="handleSendCode">获取验证码</el-button>
             </el-col>
           </el-form-item>
           <el-form-item>
-            <el-button class="btn-login" type="primary" @click="onSubmit">登陆</el-button>
+            <el-button class="btn-login" type="primary">登陆</el-button>
           </el-form-item>
         </el-form>
       </div>
@@ -27,19 +27,31 @@
 </template>
 
 <script>
+
+import axios from 'axios'
+
 export default {
   name: 'AppLogin',
   data () {
     return {
       form: {
-        mobile: '',
+        mobile: '17635950228',
         code: ''
       }
     }
   },
   methods: {
-    onSubmit () {
-      console.log('submit!')
+
+    // 请求获取初始化验证码参数
+    handleSendCode () {
+      // console.log('handleSendCode')
+      let { mobile } = this.form
+      axios({
+        method: 'GET',
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+      }).then(res => {
+        console.log(res.data)
+      })
     }
   }
 }
