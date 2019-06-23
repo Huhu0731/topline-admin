5c7211600018f58a58cfc1553b9f640e10a0ae7e
Huhu0731 <you@example.com>
2019-06-23 23:17:54 +0800

    layout组件-创建并配置路由

---
+++ src/views/layout/components/AppAside.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>aside component</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'AppAsider',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/layout/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>layout component</p>
+        <AppHeader/>
+        <AppAside/>
+    </div>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader'
+import AppAside from './components/AppAside'
+
+export default {
+  name: 'AppLayout',
+  components: {
+    AppAside,
+    AppHeader
+  },
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
