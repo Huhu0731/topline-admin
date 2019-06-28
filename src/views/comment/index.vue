6e4801f604b2ac946d954e9a333e2508297a3a65
Huhu0731 <you@example.com>
2019-06-29 00:58:57 +0800

    评论管理--修改文章评论状态&&处理开关禁用

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -12,9 +12,11 @@
         label="允许评论">
         <template slot-scope="scope">
           <el-switch
+            :disabled="scope.row.changeLoading"
             v-model="scope.row.comment_status"
             active-color="#13ce66"
-            inactive-color="#ff4949">
+            inactive-color="#ff4949"
+            @change="handleChangeCommentStatus(scope.row)">
           </el-switch>
         </template>
       </el-table-column>
@@ -46,10 +48,42 @@ export default {
         }
       }).then(data => {
         // console.log(data)
+        // 获取文章时候 都让按钮的加载状态为false
+        data.results.forEach(item => {
+          item.changeLoading = false
+        })
         this.articles = data.results
       }).catch(err => {
         console.log(err)
         this.$message.error('加载评论列表失败')
+      })
+    },
+    // 修改文章评论状态
+    handleChangeCommentStatus (item) {
+      // 没办法一个一个评论状态禁用
+      // 就给每个对象添加一个属性 用来控制网络慢时的禁用状态
+      item.changeLoading = true // 禁用开关的点击状态
+      this.$http({
+        method: 'PUT',
+        url: '/comments/status',
+        params: {
+          article_id: item.id.toString()
+        },
+        data: {
+          allow_comment: item.comment_status
+        }
+      }).then(data => {
+        console.log(data)
+        this.$message({
+          type: 'success',
+          message: `${item.comment_status ? '启用' : '关闭'}评论状态成功`
+        })
+        item.changeLoading = false // 启用开关的点击状态
+      }).catch(err => {
+        console.log(err)
+        this.$message.error('评论状态修改失败')
+        item.changeLoading = false // 启用开关的点击状态
+        item.comment_status = !item.comment_status // 如果有错则返回原来状态
       })
     }
   }
